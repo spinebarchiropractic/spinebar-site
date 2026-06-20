c626217843bea5794e058754a0d3e06cb206f436
spinebarchiropractic <spinebarclinic@gmail.com>
2026-06-20 12:41:05 -0700

    Fix robots.ts sitemap reference to www.spinebar.com

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://spinebar.com/sitemap.xml",
+    sitemap: "https://www.spinebar.com/sitemap.xml",
   };
 }
